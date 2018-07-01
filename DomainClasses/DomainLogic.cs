a68495825380d685b7f1fb0368f0340efd3befe9
Philop <f.paracka@gmail.com>
2018-07-01 19:12:45 +0200

    New commit

---
+++ DomainClasses/DomainLogic.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainClasses
+{
+    public class DomainLogic
+    {
+        public bool ValidateUser(string name, string password)
+        {
+            using (var context = new Entities())
+            {
+                var result = context.USERS.Where(x => x.NAME == name && x.PASSWORD == password).Any();
+                return result;
+            }
+        }
+
+        public decimal GetUserID(string name)
+        {
+            using (var context = new Entities())
+            {
+                var result = context.USERS.Where(x => x.NAME == name).Select(x => x.ID).FirstOrDefault();
+                return result;
+            }
+        }
+    }
+}
---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DomainClasses;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DomainLogic dl = new DomainLogic();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            if (dl.ValidateUser(textBox.Text,passwordBox.Password.ToString()))
+            {
+                MessageBox.Show("U nigga rules");
+                Global.UserID = dl.GetUserID(textBox.Text);
+                Global.Username = textBox.Text;
+            }
+        }
+    }
+}
